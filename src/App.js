3dce3b5127cc2dbbb8955ed6016b5b0b74fdd10c
Jairo Cruz <azaveliz@gmail.com>
2022-03-24 16:08:36 -0600

    add layout, and other library

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
+//import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './views/home';
+//import Web3 from 'web3'; // Esto me dio error, la linea de abajo es funcional con webpack >= 5
+ import Web3 from "web3/dist/web3.min";
+import MainLayout from './layouts/main';
 
 
 function App() {
+
+  // useEffect(()=> {
+  //   if(window.ethereum) {
+  //     /* window.ethereum.request({
+  //       method: "eth_requestAccounts",
+  //     })
+  //     .then((accounts) => console.log(accounts)); */
+  //     // this is with web3
+  //     const web3 = new Web3(window.ethereum);
+  //     web3.eth.requestAccounts().then(console.log);
+  //   } else {
+  //     console.log("debe instalar algun proveedor como metamask para continual");
+  //   }
+  // }, []);
+
   return (
-    <>
+    <MainLayout>
     <Routes>
       <Route path="/" exact element={<Home/>}/>
     </Routes>
-      
-    </>
+    </MainLayout>
     
   );
 }
 
 export default App;
+
+
