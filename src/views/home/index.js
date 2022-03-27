299ee78a2d3c5d8e9e3f7167b0972e24413fbea1
jairo <azaveliz@gmail.com>
2022-03-27 17:17:16 -0600

    Connect with contract and wallet is working

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,7 +1,33 @@
+import { useWeb3React } from "@web3-react/core";
+import { useCallback, useEffect, useState } from "react";
+import usePlatMark from "../../hooks/usePlatMark";
+
+
+
 const Home = () => {
+
+    const { active } = useWeb3React();
+
+    const [maxSupply, setMaxSupply] = useState();
+
+    const platMark = usePlatMark();
+
+    const getMaxSupply = useCallback(async () => {
+        if (platMark) {
+            const result = await platMark.methods.maxSupply().call();
+            setMaxSupply(result);
+        }
+    }, [platMark]);
+
+    useEffect(()=> {
+        getMaxSupply();
+    }, [getMaxSupply]);
+
+    if (!active) return "Conecta tu wallet";
+
     return(
         <div>
-            <p>Hello World jairo</p>
+            <p>Max supply: {maxSupply}</p>
         </div>
     );
 }
